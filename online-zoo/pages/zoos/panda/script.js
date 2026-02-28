82bf152abdfe11ea7c6df1d64098ba329c412276
Islom <islom.boynazarov@gmail.com>
2026-02-28 08:29:34 +0500

    feat make panda page responsive for 640 up to purple belt

--- online-zoo/pages/zoos/panda/script.js
+++ online-zoo/pages/zoos/panda/script.js
@@ -32,17 +32,3 @@ prevBtn.addEventListener('click', () => {
 });
 
 
-
-// Hamburger menu
-
-document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.getElementById('burger');
-  const nav = document.querySelector('.header__nav');
-
-  if (burger && nav) {
-    burger.addEventListener('click', () => {
-      burger.classList.toggle('open');
-      nav.classList.toggle('open');
-    });
-  }
-});
